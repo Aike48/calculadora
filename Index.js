2bbae2f22773f5591b02c4ff6618808a8dae0d05
Michel Pérez <SENA@ARMAPRCCIFOD605.FormacionSena.RED>
2024-02-22 08:11:01 -0500

    Implemetacion de la operacion de multiplicacion

--- Index.js
+++ Index.js
@@ -6,7 +6,11 @@ function sumar(num1, num2) {
 function restar(num1, num2) {
     return num1 - num2;
   }  
-  
+
+function multiplicar(num1, num2) {
+    return num1 * num2;
+  }  
+
   // Función principal del programa
   function calculadora() {
     var opcion = prompt(
